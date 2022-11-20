43ebac646f19b02fe4895c35c20733fcaa5fdc94
Koen Werdler <koen.werdler@sentia.com>
2022-11-20 21:18:12 +0100

    Script to build and push docker image

---
+++ build-and-push.sh
@@ -0,0 +1,4 @@
+docker build -t werdlerk/battlesnake:latest .
+docker login
+docker push werdlerk/battlesnake:latest
+# When you get error "denied: requested access to the resource is denied", try docker logout, docker login
